959d08b6b90f2fedc692c0a171602ef9f67d79e4
Snakefra <158300295+Snakefra@users.noreply.github.com>
2024-07-31 23:19:01 -0600

    Create ADC.h

---
+++ Digital2_lab3SL/Digital2_lab3SL/ADC/ADC.h
@@ -0,0 +1,19 @@
+
+/*
+ * ADC.h
+ *
+ * Created: 31/07/2024 14:25:59
+ *  Author: Snake
+ */ 
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <avr/io.h>
+#include <stdint.h>
+#define F_CPU 16000000
+#include <util/delay.h>
+
+void initADC();
+
+
+#endif
